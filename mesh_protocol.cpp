5d7691ce4395787780aca0a7e35d1f6d1326becf
unknown <daniel.fay@gmail.com>
2019-03-18 21:11:06 -0600

    Added in function to check if we've seen this packet before.

--- mesh_protocol.cpp
+++ mesh_protocol.cpp
@@ -1,5 +1,25 @@
+/*
+ * Copyright (c) 2019, Daniel R. Fay.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "mbed.h"
 #include "serial_data.hpp"
+#include "params.hpp"
+#include <list>
+#include <algorithm>
 
 extern FrameQueue tx_queue, rx_queue;
 
@@ -37,4 +57,21 @@ void rxCallback(frame_t *rx_frame) {
     rx_mesh_time.attach(txCallback, TIME_SLOT_SECONDS);
     rx_queue.enqueue(rx_frame);
     memcpy(&mesh_data, rx_frame, sizeof(frame_t));
+}
+
+
+static list<uint32_t> crc_list_vals;
+bool checkRedundantPkt(const uint8_t *pld, const size_t len) {
+    bool ret_val = false;
+    MbedCRC<POLY_32BIT_ANSI, 32> ct;
+    uint32_t crc = 0;
+    ct.compute((void *) pld, len, &crc);
+    if(find(crc_list_vals.begin(), crc_list_vals.end(), crc) == crc_list_vals.end()) {
+        ret_val = true;
+        crc_list_vals.push_back(crc);
+        if(crc_list_vals.size() > PKT_CHK_HISTORY) {
+            crc_list_vals.pop_front();
+        }
+    }
+    return ret_val;
 }
