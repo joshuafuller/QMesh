f6a85e661ba1dc85ad2720faae20350d35ad5159
Dan Fay <daniel.fay@gmail.com>
2019-07-20 09:38:09 -0600

    Small fixes

--- json_serial.hpp
+++ json_serial.hpp
@@ -21,21 +21,21 @@ class JSONSerial {
 protected:
     MbedJSONValue rx_json;
 public:
-// Converts a frame into JSON, with the frame being converted into Base64
+// Converts a frame into JSON, with the serialized frame being converted into Base64
 void frameToJSON(Frame &frame, string &json_str) {
     size_t frame_buf_len = frame.getFullPacketSize():
     uint8_t *frame_buf = malloc(frame_buf_len);
     MBED_ASSERT(frame.serialize(frame_buf) > 0);
 
     MbedJSONValue json_val;
-    size_t b64_len;
-    int base64_size = mbedtls_base64_encode(NULL, 0, &b64_len, frame_buf, frame_buf_len);
-    unsigned char b64_buf = malloc(b64_len);
+    MBED_ASSERT(mbedtls_base64_encode(NULL, 0, &b64_len, frame_buf, frame_buf_len) == 0);
+    unsigned char *b64_buf = malloc(b64_len);
+    MBED_ASSERT(mbedtls_base64_encode(b64_buf, b64_len, &b64_len, frame_buf, frame_buf_len) == 0);
     json_val["Type"] = "Frame";
     json_val["Data"] = b64_buf;
     json_str = json_val.serialize();
         
-    free(buf);
+    free(frame_buf);
     free(b64_buf);
 }
 
@@ -48,12 +48,21 @@ void settingToJSON(string &setting, string &value, string &json_str) {
     json_str = json_val.serialize();
 }
 
-// Creates a JSON-formatted string for a debug printf
+// Creates a JSON-formatted string for a debug printf, with the message being
+//  encoded as Base64
 void dbgPrintfToJSON(string &dbg_msg, string &json_str) {
     MbedJSONValue json_val;
     json_val["Type"] = "Debug Msg";
-    json_val["Message"] = dbg_msg;
+
+    MbedJSONValue json_val;
+    size_t b64_len;
+    MBED_ASSERT(mbedtls_base64_encode(NULL, 0, &b64_len, dbg_msg.c_str(), dbg_msg.size()) == 0);
+    unsigned char *b64_buf = malloc(b64_len);
+    MBED_ASSERT(mbedtls_base64_encode(b64_buf, b64_len, &b64_len, dbg_msg.c_str(), dbg_msg.size()) == 0);
+    json_val["Message"] = b64_buf;
     json_str = json_val.serialize();
+
+    free(b64_buf);
 }
 
 // Loads a JSON-formatted string into the internal data structures
@@ -69,7 +78,6 @@ void getType(string &type_str) {
 // Loads a Frame from the JSON string
 PKT_STATUS_ENUM getFrame(Frame &frame) {
     MBED_ASSERT(getType() == "Frame");
-    size_t b64_len;
     string b64_str = rx_json["Data"].get<string>();
     MBED_ASSERT(mbedtls_base64_decode(NULL, 0, &b64_len, b64_str.c_str(), b64_str.size()) == 0);
     uint8_t *frame_data = malloc(b64_len);
