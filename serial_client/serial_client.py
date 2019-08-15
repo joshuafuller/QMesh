5d36af4dfd76df035880f09e5c96bc15e23fccb7
Dan Fay <daniel.fay@gmail.com>
2019-08-15 06:23:51 -0600

    Basic support for receiving and parsing debug printf messages from the board

--- serial_client/serial_client.py
+++ serial_client/serial_client.py
@@ -1,18 +1,22 @@
 import serial
 import sys, os
 import logging
+import json
+import base64
 
 
+# Open the serial port
+serial_port = sys.argv[1]
+ser = serial.Serial(serial_port, baudrate=921600)
 
-print("Now opening the selected serial port")
-while(True):
-	print("Please select a serial port")
-	ser_port = input()
-	print("You selected COM" + str(ser_port))	
-	print("Now opening the selected serial port")
-	try: 
-		ser = serial.Serial('COM' + str(ser_port), 115200)
-		break
-	except serial.SerialException: 
-		print("ERROR: Cannot open serial port.")
-		print("")
+while True:
+    line = ser.getLine()
+    parsed_line = json.loads(line)
+    if parsed_line["Type"] == "Debug Msg":
+        msg = base64.b64decode(unparsed_line["Message"])
+        print(msg);
+    elif parsed_line["Type"] == "Status":
+        status = parsed_line["Status"]
+        value = parsed_line["Value"]
+        print("Status Msg: %s, %s" % (status, value))
+
