a3d6655162aa797524775458a5d941c7125d06b0
Dan <daniel.fay@gmail.com>
2021-11-24 18:24:06 -0700

    Compiled with the address sanitizer

--- test/test_fec.cpp
+++ test/test_fec.cpp
@@ -10,11 +10,20 @@
 using namespace std;
 
 int main() {
+    cout << "================================================================================" << endl;
+    cout << "NOW TESTING FECRSV" << endl;
     test_fec<FECRSV>();
+    cout << "================================================================================" << endl;
+    cout << "NOW TESTING FECConv" << endl;
     test_fec<FECConv>();
+    cout << "================================================================================" << endl;
+    cout << "NOW TESTING FECInterleave" << endl;
     test_fec<FECInterleave>();
+#if 0
+    cout << "================================================================================" << endl;
+    cout << "NOW TESTING FECRSVGolay" << endl;
     test_fec<FECRSVGolay>();
-
+#endif
     return 0;
 }
 
