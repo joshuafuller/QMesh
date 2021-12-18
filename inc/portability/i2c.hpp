23412553e32d2e43c9a4a19e164ac19ced9a7107
Dan <daniel.fay@gmail.com>
2021-12-18 08:58:06 -0700

    First pass at adding the I2C code

--- inc/portability/i2c.hpp
+++ inc/portability/i2c.hpp
@@ -1,6 +1,7 @@
 #ifndef I2C_HPP
 #define I2C_HPP
 
+#define ESP_IDF
 
 #if defined(MBED_OS)
 #include "mbed.h"
@@ -8,49 +9,62 @@ class SoftI2C;
 using PORTABLE_I2C = SoftI2C;
 
 #elif defined(ESP_IDF)
-#include "Arduino.h" // Using the Arduino library here
-#include <spi.h>
-class PORTABLE_SPI {
+#include "driver/i2c.h"
+
+#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+
+int i2c_master_port = 0;
+i2c_config_t conf = {
+    .mode = I2C_MODE_MASTER,
+    .sda_io_num = I2C_MASTER_SDA_IO,         // select GPIO specific to your project
+    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+    .scl_io_num = I2C_MASTER_SCL_IO,         // select GPIO specific to your project
+    .scl_pullup_en = GPIO_PULLUP_ENABLE,
+    .master.clk_speed = I2C_MASTER_FREQ_HZ,  // select frequency specific to your project
+    // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+};
+
+class PORTABLE_I2C {
 private:
-    SPISettings settings;
+    int i2c_master_port;
+    i2c_config_t cfg;
 public:
-    SPI(PinName mosi, PinName miso, PinName sclk, PinName ssel) {     
-        SPI.begin();
+    PORTABLE_I2C(PinName scl, PinName sda, PinName sclk, PinName ssel) {     
+        i2c_master_port = I2C_MASTER_NUM;
+
+        cfg.mode = I2C_MODE_MASTER,
+        cfg.sda_io_num = I2C_MASTER_SDA_IO,
+        cfg.scl_io_num = I2C_MASTER_SCL_IO,
+        cfg.sda_pullup_en = GPIO_PULLUP_ENABLE,
+        cfg.scl_pullup_en = GPIO_PULLUP_ENABLE,
+        cfg.master.clk_speed = I2C_MASTER_FREQ_HZ,
+
+        i2c_param_config(i2c_master_port, &conf);
+
+        PORTABLE_ASSERT(i2c_driver_install(i2c_master_port, conf.mode, 
+                                I2C_MASTER_RX_BUF_DISABLE, 
+                                I2C_MASTER_TX_BUF_DISABLE, 0) == ESP_OK);
     }
 
-    ~SPI() {
-        SPI.end();
+    ~PORTABLE_I2C() {
+        i2c_driver_delete(i2c_master_port);
     }
-
-    void format(int bits, int mode = 0) {
-        if(mode == 0) {
-            settings.dataMode = SPI_MODE0;
-        } else if(mode == 1) {
-            settings.dataMode = SPI_MODE1;
-        } else if(mode == 2) {
-            settings.dataMode = SPI_MODE2;
-        } else if(mode == 3) {
-            settings.dataMode = SPI_MODE3;
-        } else {
-            MBED_ASSERT(false);
-        }
-
-        settings.dataOrder = MSGFIRST;
-    }
-
-    void frequency(int hz=1000000) { 
-        settings.speedMaximum = hz;
-    }
-
+ 
     void lock() { }
 
     void unlock() { }
 
-    write(int val) -> int {
-        SPI.beginTransaction(settings);
-        int ret_val = spi.transfer(val);
-        SPI.endTransaction(settings);
-        return ret_val;
+    auto write(int address, const char *data, int length, bool repeated=false) -> int {
+        PORTABLE_ASSERT(repeated == false);
+        PORTABLE_ASSERT(length <= 256 && length > 0);
+        PORTABLE_ASSERT(i2c_master_write_to_device(i2c_master_port, address, data, length, 
+            I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS) == ESP_OK);
     }
 };
 #else
